e18f80fce10776e8751ae99d4766c1a4f9b5cde8
eslenaa <eslenaa@gmail.com>
2024-10-20 20:11:28 +0200

    AddArrowBackButton

--- app/src/main/java/com/viu/actividad1/views/screens/NewTripScreen.kt
+++ app/src/main/java/com/viu/actividad1/views/screens/NewTripScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Luggage
 import androidx.compose.material3.Icon
@@ -54,6 +55,24 @@ fun NewTripScreen(
         Column(modifier = Modifier.padding(contentPadding),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Start
+            ){
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIosNew,
+                    contentDescription = "Atrás",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable {
+                            navController.popBackStack()
+                        },
+                    tint = tertiaryLight
+                )
+            }
             Row(
                 modifier = Modifier
                     .padding(10.dp)
